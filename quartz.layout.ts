b5209584e1feb006dd3559fb1e39f5156d1eb726
jinju0323 <tlawlswn28@naver.com>
2025-01-16 03:56:01 +0900

    Update Giscus

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -12,7 +12,7 @@ export const sharedPageComponents: SharedLayout = {
         // data-repo
         repo: 'jinju0323/blog',
         // data-repo-id
-        repoId: 'R_kgDOLKhz-Q',
+        repoId: 'R_kgDONqhZ1g',
         // data-category
         category: 'Announcements',
         // data-category-id
